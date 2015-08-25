be409a8c885a7a96539f8874949671cd14b2d542
Seth Duncan <sethduncan@gmail.com>
2015-08-25 02:16:07 -0500

    Any group created on the front end will, whenever an input is blurred, create an object with all the related group items' data in it and pass it to the relevant calculator.

--- main.js
+++ main.js
@@ -3,12 +3,24 @@ $(document).ready(function() {
   // auto-calculate and populate all related form fields when possible
   $('form input').blur(function () {
       var group = $(this).data("group");
-      doCalculations(group);
+      var groupData = {};
+      $("form ."+group).each(function(index) {
+        var label = $(this).attr("placeholder");
+        var value = $(this).val();
+        if (!value) { value="0"; }
+        Object.defineProperty(groupData, label, {
+          value: value,
+          writable: true,
+          enumerable: true,
+          configurable: true
+        });
+      });
+      doCalculations(group, groupData);
   });
 
 });
 
 // The Meat & Potatoes calculations
-function doCalculations (group) {
-
+function doCalculations (group, groupData) {
+  if (group == "planet") { console.log(groupData.Radius); }
 }
