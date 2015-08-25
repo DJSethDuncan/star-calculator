918c8749b656ad583671f494d621de01e31b1547
Seth Duncan <sethduncan@gmail.com>
2015-08-25 02:29:53 -0500

    Basic cleanup.

--- main.js
+++ main.js
@@ -2,12 +2,18 @@ $(document).ready(function() {
 
   // auto-calculate and populate all related form fields when possible
   $('form input').blur(function () {
+
       var group = $(this).data("group");
       var groupData = {};
+
+      // iterate over every item in the same group
       $("form ."+group).each(function(index) {
-        var label = $(this).attr("placeholder");
+
+        var label = $(this).attr("name");
         var value = $(this).val();
         if (!value) { value="0"; }
+
+        // create an object with all values inside the same group that was triggered
         Object.defineProperty(groupData, label, {
           value: value,
           writable: true,
@@ -15,12 +21,26 @@ $(document).ready(function() {
           configurable: true
         });
       });
-      doCalculations(group, groupData);
+
+      groupData.group = group;
+
+      // pass the data along
+      doCalculations(groupData);
   });
 
 });
 
+function doCalculations (groupData) {
+  if (groupData.group == "planet") {
+    var planetData = new Planet(groupData);
+    console.log(planetData);
+  }
+}
+
+
 // The Meat & Potatoes calculations
-function doCalculations (group, groupData) {
-  if (group == "planet") { console.log(groupData.Radius); }
+function Planet (data) {
+  this.radius = data.planetRadius;
+  this.circumference = data.planetCircumference;
+  this.diameter = data.planetDiameter;
 }
