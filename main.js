1f7b287648371a49f8ad3a659c563b838cae3128
DJSethDuncan <sethduncan@gmail.com>
2015-08-26 14:20:11 -0500

    Converted to OOP for the data so we can use celestialBodies as an object and automate a lot of this.

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
   var wichitaLon = "-97.3361";
   var nasaAPIKey = "xiTgDdCaqdv7NW7aTB7bby3D7424Rgl9w09y0diN";
   var imageryURL = "https://api.nasa.gov/planetary/earth/imagery?lat="+wichitaLat+"&lon="+wichitaLon+"&cloudscore=true&api_key="+nasaAPIKey+"&format=JSON";
-  var planetaryStatesURL = "http://www.astro-phys.com/api/de406/states?bodies=mercury,venus,earth,moon,mars"
+  var planetaryStatesURL = "http://www.astro-phys.com/api/de406/states?bodies=mercury,venus,earth,moon,mars,jupiter,saturn,uranus,neptune,pluto"
 
   // imagery for given location
   $.ajax({
@@ -18,38 +18,42 @@ $(document).ready(function() {
 
   $.ajax({
     url: planetaryStatesURL,
-    success: function(data) { /* console.log(data); */ }
+    success: function(data) { console.log("success"); }
   }).done(function(data) {
-    var mercury = new Object();
-    var venus = new Object();
-    var earth = new Object();
-    var moon = new Object();
-    var mars = new Object();
 
-    mercury.x = data.results.mercury[0][0];
-    mercury.y = data.results.mercury[0][1];
-    mercury.z = data.results.mercury[0][2];
-    venus.x = data.results.venus[0][0];
-    venus.y = data.results.venus[0][1];
-    venus.z = data.results.venus[0][2];
-    earth.x = data.results.earth[0][0];
-    earth.y = data.results.earth[0][1];
-    earth.z = data.results.earth[0][2];
-    moon.x = data.results.moon[0][0];
-    moon.y = data.results.moon[0][1];
-    moon.z = data.results.moon[0][2];
-    mars.x = data.results.mars[0][0];
-    mars.y = data.results.mars[0][1];
-    mars.z = data.results.mars[0][2];
+    // set reference point for comparison to other planets
+    // later on this should be able to be chosen
+    var earth = {
+      x : data.results.earth[0][0],
+      y : data.results.earth[0][1],
+      z : data.results.earth[0][2]
+    }
 
-    var distanceToMercury = distanceBetween(earth, mercury);
-    var distanceToVenus = distanceBetween(earth, venus);
-    var distanceToMoon = distanceBetween(earth, moon);
-    var distanceToMars = distanceBetween(earth, mars);
-    $("#distanceToMercury").html("Mercury: "+distanceToMercury);
-    $("#distanceToVenus").html("Venus: "+distanceToVenus);
-    $("#distanceToMoon").html("Moon: "+distanceToMoon);
-    $("#distanceToMars").html("Mars: "+distanceToMars);
+    // create the object
+    var celestialBodies = {};
+
+    // add each body returned from the API to the celestialBodies object
+    $.each(data.results, function(label) {
+      var thisBody = {
+        x: data.results[label][0][0],
+        y: data.results[label][0][1],
+        z: data.results[label][0][2]
+      }
+      // calculate the distance between this body and earth (our reference point)
+      distance = distanceBetween(thisBody, earth);
+      // add the distance to this body's object
+      thisBody.distanceToEarth = distance;
+      // add this body's object to the celestialBodies object
+      celestialBodies[label] = thisBody;
+    });
+
+    // write each celestial body distance to an <li>
+    $.each(celestialBodies, function(label) {
+      if (label != "earth") {
+        $("#distances").append("<li id="+label+">"+label+": "+celestialBodies[label].distanceToEarth+"</li>");
+      }
+    });
+
   });
 
   // Constants
