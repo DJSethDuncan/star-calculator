7919ba15454bf48ee1f7c51d3471aeab1060b69f
Seth Duncan <sethduncan@gmail.com>
2015-08-25 19:16:09 -0500

    Added gravity calculations. Calculates a planet's gravity in m/s as well as the comparative gravity to Earth

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
 
+  // Constants
+
+  var G = 6.674*Math.pow(10, -11);
+  var earthMass = 5.98*Math.pow(10, 24);
+  var earthRadius = 6.38*Math.pow(10, 6);
+  var earthGravity = 9.8;
+  
   // auto-calculate and populate all related form fields when possible
   $('form input').blur(function () {
 
@@ -51,6 +58,13 @@ $(document).ready(function() {
       this.volume = ((4/3)*Math.PI*Math.pow(data.planetRadius, 3)).toFixed(2);
       this.circumference = (2*Math.PI*data.planetRadius);
     }
+
+    if (data.planetRadius != 0 && data.planetMass != 0) {
+      var planetRadiusMeters = data.planetRadius*1000;
+      this.gravity = (((G*data.planetMass)/Math.pow(planetRadiusMeters, 2)).toFixed(2));
+      this.comparativeGravity = (this.gravity/earthGravity).toFixed(2);
+    }
   }
+
 
 });
