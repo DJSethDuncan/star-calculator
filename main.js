caa84aa9075016ac61a52a3c600a86b2516699e3
Seth Duncan <sethduncan@gmail.com>
2015-08-26 02:27:48 -0500

    Added functions to call positions on planets from astro-phys.com API and calculate distances

--- main.js
+++ main.js
@@ -1,5 +1,37 @@
 $(document).ready(function() {
 
+  var planetaryStatesURL = "http://www.astro-phys.com/api/de406/states?bodies=mercury,venus,earth,mars"
+
+  $.ajax({
+    url: planetaryStatesURL,
+    success: function(data) { /* console.log(data); */ }
+  }).done(function(data) {
+    var mercury = new Object();
+    var venus = new Object();
+    var earth = new Object();
+    var mars = new Object();
+
+    mercury.x = data.results.mercury[0][0];
+    mercury.y = data.results.mercury[0][1];
+    mercury.z = data.results.mercury[0][2];
+    venus.x = data.results.venus[0][0];
+    venus.y = data.results.venus[0][1];
+    venus.z = data.results.venus[0][2];
+    earth.x = data.results.earth[0][0];
+    earth.y = data.results.earth[0][1];
+    earth.z = data.results.earth[0][2];
+    mars.x = data.results.mars[0][0];
+    mars.y = data.results.mars[0][1];
+    mars.z = data.results.mars[0][2];
+
+    var distanceToMercury = distanceBetween(earth, mercury);
+    var distanceToVenus = distanceBetween(earth, venus);
+    var distanceToMars = distanceBetween(earth, mars);
+    $("#distanceToMercury").html("Mercury: "+distanceToMercury);
+    $("#distanceToVenus").html("Venus: "+distanceToVenus);
+    $("#distanceToMars").html("Mars: "+distanceToMars);
+  });
+
   // auto-calculate and populate all related form fields when possible
   $('form input').blur(function () {
 
@@ -34,6 +66,18 @@ $(document).ready(function() {
       doCalculations(groupData);
   });
 
+  // get distance between points
+  // each point should be an object with x, y, z values
+  // uses KM
+  function distanceBetween(point1, point2) {
+    result = addCommas(Math.sqrt((Math.pow((point1.x-point2.x), 2))+(Math.pow((point1.y-point2.y), 2))+(Math.pow((point1.z-point2.z), 2))).toFixed(2))
+    return result+" km";
+  }
+
+  // for pretty number formatting
+  function addCommas(x) {
+      return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
 
   function doCalculations (groupData) {
     if (groupData.group == "planet") {
