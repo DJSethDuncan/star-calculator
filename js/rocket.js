8cbc26ebc6a66d0995dd63c0ed41c7c947f3f4c3
Seth Duncan <sethduncan@gmail.com>
2017-09-13 00:07:15 -0500

    Add another chart, more details

--- js/rocket.js
+++ js/rocket.js
@@ -1,36 +1,81 @@
 $('#launchRocket').click(function() {
 	var rocket = new Rocket('falcon9');
+	$('#telemetry').slideDown();
+	$('#charts').slideDown();
 	rocket.launch();
 });
 
 
-var speedGraph = new Highcharts.chart('container', {
-
-    title: {
-        text: ''
-    },
-    yAxis: {
-			labels: {
-				style: {
-					color: Highcharts.getOptions().colors[1]
-				}
+var altitudeChart = new Highcharts.chart('altitudeChart', {
+	title: {
+		text: 'Altitude'
+	},
+  yAxis: {
+		title: {
+			text: "Altitude (m)"
+		},
+		labels: {
+			style: {
+				color: Highcharts.getOptions().colors[1]
 			}
-    },
-    plotOptions: {
-			line: {
-				marker: {
-					enabled: false
-				}
-			},
-			series: {
-				pointStart: 0
+		}
+	},
+	xAxis: {
+		title: {
+			text: "Time (sec)"
+		}
+	},
+	plotOptions: {
+		line: {
+			marker: {
+				enabled: false
 			}
 		},
-    series: [{
-        name: 'Speed',
-        data: []
-    }]
+		series: {
+			pointStart: 0
+		}
+	},
+	series: [{
+		name: 'Altitude',
+		data: [],
+		showInLegend: false
+	}]
+});
 
+var velocityChart = new Highcharts.chart('velocityChart', {
+	title: {
+		text: 'Velocity'
+	},
+  yAxis: {
+		title: {
+			text: "Velocity (m/s)"
+		},
+		labels: {
+			style: {
+				color: Highcharts.getOptions().colors[1]
+			}
+		}
+	},
+	xAxis: {
+		title: {
+			text: "Time (sec)"
+		}
+	},
+	plotOptions: {
+		line: {
+			marker: {
+				enabled: false
+			}
+		},
+		series: {
+			pointStart: 0
+		}
+	},
+	series: [{
+		name: 'Velocity',
+		data: [],
+		showInLegend: false
+	}]
 });
 
 class Rocket {
@@ -75,31 +120,34 @@ class Rocket {
 	launch () {
 		var flightTime = 0;
 		var velocity = 0;
+		var prettyVelocity = 0;
 		var acceleration = this.acceleration(this.mass);
 		var activeMass = this.mass;
 		var thisRocket = this;
 		var percentFuelRemaining = 100;
 		var gForce = 0;
+		var altitude = 0;
+		var interval = 500; // cycle time in ms
 
 		var launchEvent = setInterval(function() {
 			if (flightTime < thisRocket.burnTime) {
 				activeMass = activeMass - (thisRocket.burnRate());
 				velocity = flightTime * thisRocket.acceleration(activeMass);
+				altitude += velocity;
+				prettyVelocity = velocity.toFixed(2);
 				percentFuelRemaining = ((100-(((thisRocket.burnRate() * flightTime)/thisRocket.propellantMass)*100).toFixed(1)).toFixed(1));
 				gForce = (1 + (thisRocket.acceleration(activeMass))/9.8).toFixed(1);
 
-				speedGraph.series[0].addPoint(velocity);
-				$('#liveVelocityms').html("Velocity: " + velocity.toFixed(2) + " m/s");
-				$('#liveVelocitymph').html("Velocity: " + (velocity*2.23694).toFixed(2) + " mph");
-				$('#liveMass').html("Mass: " + activeMass.toFixed(2) + " kg");
-				$('#livePercentFuelRemaining').html("Fuel: " + percentFuelRemaining + "%");
-				$('#liveAcceleration').html((thisRocket.acceleration(activeMass)).toFixed(1) + " m/s");
-				$('#liveGForce').html("G: " + gForce);
+				altitudeChart.series[0].addPoint(altitude);
+				velocityChart.series[0].addPoint(velocity);
+				$('#liveVelocity').html(velocity.toFixed(2) + " m/s<br />" + (velocity*2.23694).toFixed(2) + " mph");
+				$('#liveMass').html(activeMass.toFixed(2) + " kg");
+				$('#liveAltitude').html(altitude.toFixed(1) + " m<br />" + (altitude*3.28084).toFixed(1) + " ft");
+				$('#livePercentFuelRemaining').html(percentFuelRemaining + "%");
+				$('#liveAcceleration').html((thisRocket.acceleration(activeMass)).toFixed(1) + " m/s<br />" + gForce + "g");
 				flightTime++;
 			}
-			//speedGraph.series[0].addPoint(velocity, true, false);
-			//
-		}, 250);
+		}, interval);
 	}
 
 }
